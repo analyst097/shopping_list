9704ab1d7536a1f6ee85a581061033728997fb12
Aditya Sahu <aditya.sahu@sentra.world>
2023-12-30 11:08:56 +0530

    submit logic, remove item, form data management, etc.

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/widgets/new_item.dart';
-
-import '../data/dummy_items.dart';
+import '../models/grocery_item.dart';
 
 class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
@@ -11,9 +10,25 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  void addItem() {
-    Navigator.of(context)
-        .push(MaterialPageRoute(builder: (ctx) => const NewItem()));
+  final List<GroceryItem> _groceryItems = [];
+
+  void addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
+        MaterialPageRoute(builder: (ctx) => const NewItem()));
+
+    if (newItem == null) {
+      return;
+    }
+
+    setState(() {
+      _groceryItems.add(newItem);
+    });
+  }
+
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
   }
 
   @override
@@ -24,15 +39,21 @@ class _GroceryListState extends State<GroceryList> {
         actions: [IconButton(onPressed: addItem, icon: const Icon(Icons.add))],
       ),
       body: ListView.builder(
-          itemCount: groceryItems.length,
-          itemBuilder: (ctx, index) => ListTile(
-                title: Text(groceryItems[index].name),
-                leading: Container(
-                  width: 24,
-                  height: 24,
-                  color: groceryItems[index].category.color,
+          itemCount: _groceryItems.length,
+          itemBuilder: (ctx, index) => Dismissible(
+                onDismissed: (direction) {
+                  _removeItem(_groceryItems[index]);
+                },
+                key: ValueKey(_groceryItems[index].id),
+                child: ListTile(
+                  title: Text(_groceryItems[index].name),
+                  leading: Container(
+                    width: 24,
+                    height: 24,
+                    color: _groceryItems[index].category.color,
+                  ),
+                  trailing: Text(_groceryItems[index].quantity.toString()),
                 ),
-                trailing: Text(groceryItems[index].quantity.toString()),
               )),
     );
   }
